8b75555fa69caa8c9fb0fa38a771550400f1934c
Brenno Lemos <brenno@syndel.is>
2023-01-10 14:39:11 -0300

    day 6 part 2 solution

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -1,22 +1,23 @@
 use std::collections::HashSet;
 
-const PACKET_START_LENGTH: usize = 4;
+const PACKET_START_MARKER_LENGTH: usize = 4; // Part 1
+const MESSAGE_START_MARKER_LENGTH: usize = 14; // Part 2
 
 fn main() {
 
     let input = include_str!("../input.txt");
     let input_length = input.len();
 
-    for idx in 0..input_length - PACKET_START_LENGTH {
-        let idx_range = idx..idx + PACKET_START_LENGTH;
+    for idx in 0..input_length - MESSAGE_START_MARKER_LENGTH {
+        let idx_range = idx..idx + MESSAGE_START_MARKER_LENGTH;
 
         let char_range = &input[idx_range];
 
         let unique_characters: HashSet<char> = HashSet::from_iter(char_range.chars());
 
-        if unique_characters.len() == PACKET_START_LENGTH {
-            // Part 1
-            println!("{}: {char_range}", idx + PACKET_START_LENGTH);
+        if unique_characters.len() == MESSAGE_START_MARKER_LENGTH {
+            // Part 2
+            println!("{}: {char_range}", idx + MESSAGE_START_MARKER_LENGTH);
             break;
         }
     }
