8d52019cd2e0e3ba22f5a647cbc5bc08bbe9bb8c
Brenno Lemos <brenno@syndel.is>
2023-01-13 00:19:42 -0300

    day 8 part 2 solution

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -14,9 +14,13 @@ fn main() {
 
     let trees = read_into_matrix(input);
 
-    let visible_trees = calculate_visible_trees(&trees);
+    let visible_trees = calculate_visible_trees(&trees); // Part 1
 
     println!("Visible trees: {visible_trees}");
+
+    let best_scenic_score = calculate_best_scenic_score(&trees); // Part 2
+
+    println!("Best scenic score: {best_scenic_score}");
 
 }
 
@@ -69,5 +73,57 @@ fn calculate_visible_trees(tree_map: &Array<Tree, Dim2D>) -> usize {
     }
 
     visible_trees
+
+}
+
+fn calculate_best_scenic_score(tree_map: &Array<Tree, Dim2D>) -> usize {
+
+    let nb_rows = tree_map.len_of(Axis(0));
+    let nb_cols = tree_map.len_of(Axis(1));
+
+    let mut best_scenic_score = 0;
+
+    for ((row_idx, col_idx), tree) in tree_map.slice(s![1..nb_rows - 1, 1..nb_cols - 1]).indexed_iter() {
+        
+        let row_idx = row_idx + 1;
+        let col_idx = col_idx + 1;
+
+        let row = tree_map.slice(s![row_idx, ..]);
+        let col = tree_map.slice(s![.., col_idx]);
+
+        let this_tree = &tree;
+
+        let acc_scenic_score = move |(acc_score, reached_stop_point): (usize, bool), neighbor_tree: &Tree| {
+            if reached_stop_point {
+                (acc_score, true)
+            }
+            else if neighbor_tree.0 >= this_tree.0 {
+                (acc_score + 1, true)
+            }
+            else {
+                (acc_score + 1, false)
+            }
+        };
+
+        let starting_acc = (0, false);
+
+        let left_score = row.slice(s![..col_idx]).iter().rev().fold(starting_acc, acc_scenic_score);
+        let right_score = row.slice(s![col_idx + 1..]).iter().fold(starting_acc, acc_scenic_score);
+
+        let up_score = col.slice(s![..row_idx]).iter().rev().fold(starting_acc, acc_scenic_score);
+        let down_score = col.slice(s![row_idx + 1..]).iter().fold(starting_acc, acc_scenic_score);
+
+        let this_tree_score = [
+            left_score,
+            right_score,
+            up_score,
+            down_score,
+        ].iter().map(|(acc_score, _)| acc_score).product();
+
+        best_scenic_score = max(best_scenic_score, this_tree_score);
+
+    }
+
+    best_scenic_score
 
 }
