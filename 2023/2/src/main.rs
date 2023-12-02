1b4ace983d64b17fd223557717ece32f2555e64a
Brenno Lemos <dev@brenno.codes>
2023-12-02 15:20:49 -0300

    day 2023-12-2 part 2

--- 2023/2/src/main.rs
+++ 2023/2/src/main.rs
@@ -4,13 +4,18 @@ use strum::EnumString;
 
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
-    let result = cube_conundrum(INPUT);
+    let result = cube_conundrum_part_2(INPUT);
     println!("Result: {result}");
 }
 
 fn cube_conundrum(input: &str) -> i32 {
     let games = parse_games(input);
     sum_valid_games(games)
+}
+
+fn cube_conundrum_part_2(input: &str) -> i32 {
+    let games = parse_games(input);
+    sum_minimum_powers(games)
 }
 
 fn parse_games(input: &str) -> Vec<Game> {
@@ -39,6 +44,27 @@ fn game_is_valid(game: &Game) -> bool {
     }
 
     true
+}
+
+fn sum_minimum_powers<T: IntoIterator<Item = Game>>(games: T) -> i32 {
+    games
+        .into_iter()
+        .map(get_minimum_power)
+        .sum()
+}
+
+fn get_minimum_power(game: Game) -> i32 {
+    let mut minimum_red = 0;
+    let mut minimum_green = 0;
+    let mut minimum_blue = 0;
+
+    for set in game.sets {
+        minimum_red = minimum_red.max(set.red);
+        minimum_green = minimum_green.max(set.green);
+        minimum_blue = minimum_blue.max(set.blue);
+    }
+
+    minimum_red * minimum_green * minimum_blue
 }
 
 const MAX_RED_CUBE_COUNT: i32 = 12;
@@ -118,22 +144,31 @@ impl FromStr for Set {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Game, Set, cube_conundrum};
+    use crate::{Game, Set, cube_conundrum, cube_conundrum_part_2};
     use test_case::test_case;
+
+    const EXAMPLE_INPUT: &str = r"
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green        
+    ";
 
     #[test]
     fn example_case() {
-        const INPUT: &str = r"
-            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green        
-        ";
-
         const EXPECTED_OUTPUT: i32 = 8;
 
-        let result = cube_conundrum(INPUT);
+        let result = cube_conundrum(EXAMPLE_INPUT);
+
+        assert_eq!(result, EXPECTED_OUTPUT);
+    }
+
+    #[test]
+    fn example_case_part_2() {
+        const EXPECTED_OUTPUT: i32 = 2286;
+
+        let result = cube_conundrum_part_2(EXAMPLE_INPUT);
 
         assert_eq!(result, EXPECTED_OUTPUT);
     }
