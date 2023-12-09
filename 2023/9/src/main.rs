5053fce936f7df2b6f2e78c0280fda2a2e4db30d
Brenno Lemos <dev@brenno.codes>
2023-12-09 14:17:42 -0300

    day 2023-12-9 part 2

--- 2023/9/src/main.rs
+++ 2023/9/src/main.rs
@@ -1,24 +1,28 @@
 #![feature(iter_map_windows)]
+#![feature(iterator_try_reduce)]
 
 use std::{str::FromStr, num::ParseIntError};
 
 fn main() -> Result<(), SequenceParseError> {
     const INPUT: &str = include_str!("../input.txt");
-    let result = sum_predictions(INPUT)?;
-    println!("Result: {result}");
+    let (past, future) = sum_predictions(INPUT)?;
+    println!("Past: {past} | Future: {future}");
     Ok(())
 }
 
-fn sum_predictions(input: &str) -> Result<i64, SequenceParseError> {
+fn sum_predictions(input: &str) -> Result<(i64, i64), SequenceParseError> {
     input
         .trim()
         .lines()
         .filter(|line| !line.is_empty())
         .map(predict_sequence)
-        .sum()
+        .try_fold((0, 0), |(acc_past, acc_future), val| {
+            let (past, future) = val?;
+            Ok((acc_past + past, acc_future + future))
+        })
 }
 
-fn predict_sequence(line: &str) -> Result<i64, SequenceParseError> {
+fn predict_sequence(line: &str) -> Result<(i64, i64), SequenceParseError> {
     let mut values: Vec<i64> = line
         .split_whitespace()
         .map(FromStr::from_str)
@@ -27,23 +31,32 @@ fn predict_sequence(line: &str) -> Result<i64, SequenceParseError> {
 
     let mut all_zeroes = false;
     let mut last_elements = Vec::with_capacity(values.len() - 1);
+    let mut first_elements = Vec::with_capacity(values.len() - 1);
 
     while !all_zeroes {
-        let mut cur_last_element = 0;
-
         all_zeroes = true;
+        
+        first_elements.push(values.first().copied().unwrap_or_default());
+        last_elements.push(values.last().copied().unwrap_or_default());
 
         values = values
             .into_iter()
-            .inspect(|el| cur_last_element = *el)
             .map_windows(|[a, b]| b - a)
             .inspect(|sub| all_zeroes &= *sub == 0)
             .collect();
-
-        last_elements.push(cur_last_element);
     }
 
-    Ok(last_elements.into_iter().sum())
+    let past = first_elements
+        .into_iter()
+        .rev()
+        .reduce(|a, b| b - a)
+        .ok_or_else(|| unreachable!("It is guaranteed to have been populated with elements"))?;
+
+    let future = last_elements
+        .into_iter()
+        .sum();
+
+    Ok((past, future))
 
 }
 
@@ -65,8 +78,9 @@ mod tests {
 
     #[test]
     fn example_case() {
-        const EXPECTED_OUTPUT: i64 = 114;
+        const EXPECTED_PAST: i64 = 2;
+        const EXPECTED_FUTURE: i64 = 114;
         let result = sum_predictions(EXAMPLE_INPUT).unwrap();
-        assert_eq!(result, EXPECTED_OUTPUT);
+        assert_eq!(result, (EXPECTED_PAST, EXPECTED_FUTURE));
     }
 }
