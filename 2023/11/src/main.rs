f374e4b9b6fd2d243692eb72e2a33125050e3003
Brenno Lemos <dev@brenno.codes>
2023-12-11 16:40:11 -0300

    day 2023-12-11 part 2

--- 2023/11/src/main.rs
+++ 2023/11/src/main.rs
@@ -2,11 +2,11 @@ use std::collections::HashSet;
 
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
-    let result = calculate_galaxy_pairs_distances(INPUT);
+    let result = calculate_galaxy_pairs_distances(INPUT, 1_000_000);
     println!("Result: {result}");
 }
 
-fn calculate_galaxy_pairs_distances(input: &str) -> usize {
+fn calculate_galaxy_pairs_distances(input: &str, time_dialation: usize) -> usize {
     let (galaxies, expanded_rows, expanded_cols) = parse_galaxies(input);
 
     let mut distance_sum = 0;
@@ -16,6 +16,7 @@ fn calculate_galaxy_pairs_distances(input: &str) -> usize {
             let distance = distance_between_galaxies(
                 galaxy_a, galaxy_b,
                 &expanded_rows, &expanded_cols,
+                time_dialation
             );
 
             distance_sum += distance;
@@ -25,14 +26,14 @@ fn calculate_galaxy_pairs_distances(input: &str) -> usize {
     distance_sum
 }
 
-fn distance_between_galaxies(galaxy_a: &Coord, galaxy_b: &Coord, expanded_cols: &HashSet<usize>, expanded_rows: &HashSet<usize>) -> usize {
+fn distance_between_galaxies(galaxy_a: &Coord, galaxy_b: &Coord, expanded_cols: &HashSet<usize>, expanded_rows: &HashSet<usize>, time_dialation: usize) -> usize {
     let (min_x, max_x) = min_max(galaxy_a.x, galaxy_b.x);
     let (min_y, max_y) = min_max(galaxy_a.y, galaxy_b.y);
 
     (min_x+1..=max_x)
         .map(|x| {
             if expanded_cols.contains(&x) {
-                2
+                time_dialation
             } else {
                 1
             }
@@ -41,7 +42,7 @@ fn distance_between_galaxies(galaxy_a: &Coord, galaxy_b: &Coord, expanded_cols: 
             (min_y+1..=max_y)
                 .map(|y| {
                     if expanded_rows.contains(&y) {
-                        2
+                        time_dialation
                     } else {
                         1
                     }       
@@ -160,38 +161,26 @@ mod tests {
         assert_eq!(expanded_cols, expected_expanded_cols);
     }
 
-    #[test_case(5, 9, 9)]
-    #[test_case(1, 7, 15)]
-    #[test_case(3, 6, 17)]
-    #[test_case(8, 9, 5)]
-    fn test_distance_between_galaxies(idx_a: usize, idx_b: usize, expected_distance: usize) {
+    #[test_case(5, 9, 2, 9)]
+    #[test_case(1, 7, 2, 15)]
+    #[test_case(3, 6, 2, 17)]
+    #[test_case(8, 9, 2, 5)]
+    fn test_distance_between_galaxies(idx_a: usize, idx_b: usize, time_dialation: usize, expected_distance: usize) {
         let (galaxies, expanded_cols, expanded_rows) = parse_galaxies(EXAMPLE_INPUT);
 
         let galaxy_a = &galaxies[idx_a-1];
         let galaxy_b = &galaxies[idx_b-1];
 
-        let distance = distance_between_galaxies(galaxy_a, galaxy_b, &expanded_cols, &expanded_rows);
+        let distance = distance_between_galaxies(galaxy_a, galaxy_b, &expanded_cols, &expanded_rows, time_dialation);
 
         assert_eq!(distance, expected_distance);
     }
 
-    #[test]
-    fn example_case() {
-        const EXPECTED_OUTPUT: usize = 374;
-        let result = calculate_galaxy_pairs_distances(EXAMPLE_INPUT);
-        assert_eq!(result, EXPECTED_OUTPUT);
-    }
-
-    #[test]
-    fn t() {
-        const INPUT: &str = r"
-            ##
-            .#
-        ";
-
-        const EXPECTED_OUTPUT: usize = 4;
-
-        let result = calculate_galaxy_pairs_distances(INPUT);
-        assert_eq!(result, EXPECTED_OUTPUT);
+    #[test_case(2, 374)]
+    #[test_case(10, 1030)]
+    #[test_case(100, 8410)]
+    fn example_case(time_dialation: usize, expected_distance_sum: usize) {
+        let result = calculate_galaxy_pairs_distances(EXAMPLE_INPUT, time_dialation);
+        assert_eq!(result, expected_distance_sum);
     }
 }
